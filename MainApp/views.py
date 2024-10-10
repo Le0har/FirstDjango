3142fb38190ce02a19b349fabcdf0ea378609dc9
Aleksey Tikhonov <leohar@mail.ru>
2024-10-10 21:34:07 +0300

    Work in office finished

--- MainApp/views.py
+++ MainApp/views.py
@@ -18,24 +18,15 @@ def about(request):
     return render(request, 'about.html', context)
 
 
-# items = [
-#    {"id": 1, "name": "Кроссовки abibas", "quantity":5},
-#    {"id": 2, "name": "Куртка кожаная", "quantity":2},
-#    {"id": 5, "name": "Coca-cola 1 литр", "quantity":12},
-#    {"id": 7, "name": "Картофель фри", "quantity":0},
-#    {"id": 8, "name": "Кепка", "quantity":124},
-# ]
-
-
 def get_item(request, number):
-    items = Item.objects.all()
-    for item in items:
-        if item.id == number:
-            context = {'item': item}
-            return render(request, 'item_page.html', context)
-    text = f'Товар с id={number} не найден!'
-    return HttpResponse(text)
- 
+    try:
+        one_item = Item.objects.get(pk=number)
+    except Item.DoesNotExist:
+        text = f'Товар с id = {number} не найден!'
+        return HttpResponse(text)
+    else:
+        context = {'item': one_item}
+        return render(request, 'item_page.html', context)
 
 def get_list_items(request):
     items = Item.objects.all()
