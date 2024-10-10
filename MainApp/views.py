7529e2919eda65113b490178fa21d5dc4015016d
Aleksey Tikhonov <leohar@mail.ru>
2024-10-10 20:26:17 +0300

    Work in office 1-4

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from MainApp.models import Item
 
 
 def home(request):
@@ -17,18 +18,19 @@ def about(request):
     return render(request, 'about.html', context)
 
 
-items = [
-   {"id": 1, "name": "Кроссовки abibas", "quantity":5},
-   {"id": 2, "name": "Куртка кожаная", "quantity":2},
-   {"id": 5, "name": "Coca-cola 1 литр", "quantity":12},
-   {"id": 7, "name": "Картофель фри", "quantity":0},
-   {"id": 8, "name": "Кепка", "quantity":124},
-]
+# items = [
+#    {"id": 1, "name": "Кроссовки abibas", "quantity":5},
+#    {"id": 2, "name": "Куртка кожаная", "quantity":2},
+#    {"id": 5, "name": "Coca-cola 1 литр", "quantity":12},
+#    {"id": 7, "name": "Картофель фри", "quantity":0},
+#    {"id": 8, "name": "Кепка", "quantity":124},
+# ]
 
 
 def get_item(request, number):
+    items = Item.objects.all()
     for item in items:
-        if item['id'] == number:
+        if item.id == number:
             context = {'item': item}
             return render(request, 'item_page.html', context)
     text = f'Товар с id={number} не найден!'
@@ -36,6 +38,7 @@ def get_item(request, number):
  
 
 def get_list_items(request):
+    items = Item.objects.all()
     context = {'items': items}
     return render(request, 'items_list.html', context)
 
